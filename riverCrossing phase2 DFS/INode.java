30f4b2a7b2f09023fa5f94bb94e1636e53c7f286
saniyaparto981-droid <saniyaparto981@gmail.com>
2026-07-27 14:16:11 -0700

    Add files via upload

---
+++ riverCrossing phase2 DFS/INode.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public interface INode {
+
+    INode clone();
+
+    boolean equals(Object obj);
+
+    INode getMotherNode();
+
+    IState getState();
+
+    void setState(Entity[] entities);
+
+    String toString();
+}
